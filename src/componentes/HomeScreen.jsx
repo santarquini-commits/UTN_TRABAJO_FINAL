12e4f876416529b585ccd83cf5cc437487665c37
santi.dev <santarquini@gmail.com>
2026-08-04 14:39:04 -0300

    solucion imagenes

--- src/componentes/HomeScreen.jsx
+++ src/componentes/HomeScreen.jsx
@@ -8,7 +8,7 @@ function HomeScreen() {
         {
             id: 1,
             nombre: "Juan",
-            foto: <img src={`${process.env.PUBLIC_URL}/img/juan.png`} alt="Juan" />,
+            foto: `${process.env.PUBLIC_URL}/img/juan.png`,
             mensajes: [
                 { autor: "Juan", texto: "Hola, ¿cómo estás?", hora: "13:00", tipo: "recibido" }
             ]
@@ -16,7 +16,7 @@ function HomeScreen() {
         {
             id: 2,
             nombre: "María",
-            foto: <img src={`${process.env.PUBLIC_URL}/img/maria.png`} alt="María" />,
+            foto: `${process.env.PUBLIC_URL}/img/maria.png`,
             mensajes: [
                 { autor: "María", texto: "¿Listo para la reunión?", hora: "14:00", tipo: "recibido" }
             ]
